c7a9edde79f9ef73b8702850a737c86180a7bfc5
Gerard <gerarddoets@gmail.com>
2021-01-27 17:48:57 +0100

    schalen is ok

--- src/gdimageparting/GdImageParting.java
+++ src/gdimageparting/GdImageParting.java
@@ -4,62 +4,71 @@ import java.awt.Component;
 import java.awt.Container;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
+import java.awt.Image;
 import java.awt.Rectangle;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class GdImageParting {
 
-
     static JFrame frame;
     JLabel label;
-    static String starFile ;
+    static String starFile;
     static public double schaal;
+    static int imageHoogte;
 
     private void createUi(Container cnt, ImageIcon backgr) {
-        cnt.setLayout(new BoxLayout(cnt, BoxLayout.PAGE_AXIS));
+//        cnt.setLayout(new BoxLayout(cnt, BoxLayout.PAGE_AXIS));
         SelectRectangle area = new SelectRectangle(backgr, this);
         cnt.add(area);
-        label = new JLabel("");
-        label.setLabelFor(area);
-        cnt.add(label);
-        area.setAlignmentX(Component.LEFT_ALIGNMENT);
-        label.setAlignmentX(Component.LEFT_ALIGNMENT);
     }
 
-    public void updateLabel(Rectangle rect) {
-        int w = rect.width;
-        int h = rect.height;
-        if (w == 0) {
-            w = 1;
-        }
-        if (h == 0) {
-            h = 1;
-        }
-        label.setText("Rectangle extends from (" + rect.x + ", " + rect.y
-                + ") to (" + (rect.x + w - 1) + ", "
-                + (rect.y + h - 1) + ").");
+    protected static ImageIcon createBachground(String deImageFile, int frameHoogte) throws IOException {
+        File imageFile = new File(deImageFile);
+        Image imageToBeDisplayed = ImageIO.read(imageFile);
+        int heightImageToBeDisplayed = imageToBeDisplayed.getHeight(null);
+        Image imageVooricon = imageToBeDisplayed.getScaledInstance(-1, frameHoogte, Image.SCALE_FAST);
+//        return new ImageIcon(path);
+        imageHoogte = imageVooricon.getHeight(frame);
+        schaal = frameHoogte / (double) heightImageToBeDisplayed;
+
+        return new ImageIcon(imageVooricon);
     }
 
-    protected static ImageIcon createBachground(String path) {
-        return new ImageIcon(path);
-    }
-
-    private static void createGUI( ) {
+    private static void createGUI() throws IOException {
         GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
         GraphicsDevice[] schermen = ge.getScreenDevices();
         GraphicsDevice mijnScherm = schermen[0];
 
         JFrame.setDefaultLookAndFeelDecorated(true);
         frame = new JFrame("ImageParting");
+//                    if (args[0] == null){
+        JFileChooser fc = new JFileChooser();
+        int returnVal = fc.showOpenDialog(frame);
+
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = fc.getSelectedFile();
+
+//                    starFile = args[0];
+//                    starFile = args[0];
+        }
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         int frameHoogte = mijnScherm.getDisplayMode().getHeight();
         frameHoogte = frameHoogte - 100;
 
         GdImageParting controller = new GdImageParting();
-        controller.createUi(frame.getContentPane(), createBachground(starFile));
+
+        controller.createUi(frame.getContentPane(), createBachground(starFile, frameHoogte));
+        frame.setSize(imageHoogte, frameHoogte);
         frame.pack();
         frame.setVisible(true);
     }
@@ -68,11 +77,12 @@ public class GdImageParting {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                starFile = args[0];
-                createGUI();
+                try {
+                    createGUI();
+                } catch (IOException ex) {
+                    Logger.getLogger(GdImageParting.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
         });
     }
 }
-
-
